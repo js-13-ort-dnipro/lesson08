5dc14a037c189ecd90dab4a1dba07a8958c3c5eb
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2021-11-27 20:28:55 +0200

    Upload

--- src/spa-example/assets/js/app.js
+++ src/spa-example/assets/js/app.js
@@ -1,11 +1,2 @@
 
     const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
-
-    document.addEventListener('DOMContentLoaded', async function(){
-
-        let rates   = await fetch(URL);
-        rates       = await rates.json();
-
-        console.log(rates);
-
-    });
